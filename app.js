e58da610be389c0ebf313abce6eb392bf8afbc7d
SrishtiC99 <iamsri9631@gmail.com>
2022-12-29 23:08:12 +0530

    Sent Books to WebPage

--- app.js
+++ app.js
@@ -5,8 +5,25 @@ const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 
+const books = [
+  {
+    bookName: "Coolest Book Ever",
+    bookAuthor: "Srishti Kumari",
+    bookPages: 200,
+    bookPrice: 250,
+    bookState: "Available"
+  },
+  {
+    bookName: "It Ends With Us",
+    bookAuthor: "Colleen Hoover",
+    bookPages: 384,
+    bookPrice: 230,
+    bookState: "Available"
+  }
+];
+
 app.get("/", function(req, res){
-  res.render("home", {msg: "Hello I'm Srishti!!"});
+  res.render("home", {bookData: books});
 })
 
 app.listen(3000, function(){
